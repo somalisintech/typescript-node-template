981c4dcebb1a9fab9b2815c85c256bc2fb3be7ea
Abshir Ahmed <abshir.ahmed@outlook.com>
2022-10-20 23:21:06 +0100

    :arrow_up: upgrade deps, install swc

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,15 @@
-/** @type {import('@jest/types').Config.InitialOptions} */
+/** @type {import("@jest/types").Config.InitialOptions} */
 const config = {
-  // https://kulshekhar.github.io/ts-jest/docs/getting-started/presets
-  preset: 'ts-jest/presets/js-with-ts',
   testEnvironment: 'node',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   detectOpenHandles: true,
   forceExit: true,
   verbose: true,
   clearMocks: true,
-  collectCoverageFrom: ['<rootDir>/src/**/*']
+  collectCoverageFrom: ['<rootDir>/src/**/*'],
+  transform: {
+    '^.+\\.(t|j)sx?$': ['@swc/jest']
+  }
 };
 
 module.exports = config;
